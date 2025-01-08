95dc4ef0858b198db6bf4fb23ea9abe46dbb097f
qqqqqnfivvdnvn <2317140314@qq.com>
2025-01-08 21:42:47 +0800

    vue的demo

--- vue_demo/src/components/HelloWorld.vue
+++ vue_demo/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>{{ message }}</h1>
+    <p>Name: {{ name }}</p>
+    <p>Age: {{ age }}</p>
   </div>
 </template>
 
@@ -9,15 +11,22 @@ import { ref } from 'vue';
 
 export default {
   setup() {
-    const message = ref('Hello Vue 3!');
-    return { message };
+    const message = ref('Hello Vue 3.0!')
+
+    return {
+      message: message,
+      name: 'John',
+      age: 30
+    };
   }
 };
 </script>
 
-
 <style>
 h1 {
   color: #42b983;
+}
+p {
+  font-size: 16px;
 }
 </style>
